9f2f57261f446b04a8456eaf7aa1fb1441de7cec
jiachengng <70906604+jiachengng@users.noreply.github.com>
2021-10-19 17:54:31 +0800

    .

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,22 +13,35 @@ namespace Test
 {
     public static class Program
     {
-        private static string publishedModelName = "Iteration8";
+        private static string publishedModelName = "Iteration9";
+
+        // You can obtain these values from the Keys and Endpoint page for your Custom Vision resource in the Azure Portal.
+        private static string trainingEndpoint = "https://customvisionjc.cognitiveservices.azure.com/";
+        private static string trainingKey = "e7c2563942b8482595e6c80a9e960892";
+        // You can obtain these values from the Keys and Endpoint page for your Custom Vision Prediction resource in the Azure Portal.
+        private static string predictionEndpoint = "https://customvisionjc-prediction.cognitiveservices.azure.com/";
+        private static string predictionKey = "6e8dc9f09f5d4ffd9dafb9aaa33a8da2";
+
+
+        private static CustomVisionTrainingClient trainingApi = AuthenticateTraining(trainingEndpoint, trainingKey);
+        private static CustomVisionPredictionClient predictionApi = AuthenticatePrediction(predictionEndpoint, predictionKey);
+
+        private static Project project = GetProject(trainingApi);
+        private static Tag newTag;
+
+        private static List<string> newImages;
         public static void Main()
         {
-            // You can obtain these values from the Keys and Endpoint page for your Custom Vision resource in the Azure Portal.
-            string trainingEndpoint = "https://customvisionjc.cognitiveservices.azure.com/";
-            string trainingKey = "e7c2563942b8482595e6c80a9e960892";
-            // You can obtain these values from the Keys and Endpoint page for your Custom Vision Prediction resource in the Azure Portal.
-            string predictionEndpoint = "https://customvisionjc-prediction.cognitiveservices.azure.com/";
-            string predictionKey = "6e8dc9f09f5d4ffd9dafb9aaa33a8da2";
-
-
-            CustomVisionTrainingClient trainingApi = AuthenticateTraining(trainingEndpoint, trainingKey);
-            CustomVisionPredictionClient predictionApi = AuthenticatePrediction(predictionEndpoint, predictionKey);
-
-            Project project = GetProject(trainingApi);
+            
+            int task = 0;
             int weighingScaleStatus = 1;
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+
+            }
 
             //While an item is on the scale
             while (weighingScaleStatus == 1)
@@ -67,6 +80,61 @@ namespace Test
             return trainingApi.GetProject(Guid.Parse(V));
         }
 
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Add Tag");
+            Console.WriteLine("2) Add Images");
+            Console.WriteLine("3) Train Model");
+            Console.WriteLine("4) Image Taking");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddTags(trainingApi, project);
+                    return true;
+                case "2":
+                    UploadImages(trainingApi, project);
+                    return true;
+                case "3":
+                    return false;
+                case "4":
+                    TakePhotoThreading();
+                    return true;
+                default:
+                    return true;
+            }
+        }
+
+        private static void AddTags(CustomVisionTrainingClient trainingApi, Project project)
+        {
+            //List<string> newImages;
+            Console.WriteLine("Tag Name:");
+            String tag = Console.ReadLine();
+            newTag = trainingApi.CreateTag(project.Id, tag);
+        }
+
+        private static void LoadImagesFromDisk()
+        {
+            // this loads the images to be uploaded from disk into memory
+            newImages = Directory.GetFiles(Path.Combine(@"C:\Users\Admin\Downloads", "JC")).ToList();
+            //MemoryStream testImage = new MemoryStream(File.ReadAllBytes(Path.Combine("Images", "Test", "test_image.jpg")));
+        }
+
+        private static void UploadImages(CustomVisionTrainingClient trainingApi, Project project)
+        {
+            // Add some images to the tags
+            Console.WriteLine("\tUploading images");
+            LoadImagesFromDisk();
+
+            // Or uploaded in a single batch 
+            var imageFiles = newImages.Select(img => new ImageFileCreateEntry(Path.GetFileName(img), File.ReadAllBytes(img))).ToList();
+            trainingApi.CreateImagesFromFiles(project.Id, new ImageFileCreateBatch(imageFiles, new List<Guid>() { newTag.Id }));
+
+        }
+
         private static void TestIteration(CustomVisionPredictionClient predictionApi, Project project)
         {
             VideoCapture capture = new VideoCapture(); //create a camera captue
@@ -75,7 +143,7 @@ namespace Test
             //Saving photos into folder
             string filename = "file";
             image.Save(filename);
-            string FileName = System.IO.Path.Combine(@"C:\Users\jiacheng\Downloads", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            string FileName = System.IO.Path.Combine(@"C:\Users\Admin\Downloads\JC", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
             image.Save(FileName + ".jpg");
             string imageFilePath = filename;
 
@@ -94,13 +162,88 @@ namespace Test
             {
                 Console.WriteLine($"\t{result.Predictions[i].TagName}: {result.Predictions[i].Probability:P1}");
             }
-            {
+ 
+        }
+        public static void ChildThread1()
+        {
+            Console.WriteLine("Child 1 thread starts");
+            VideoCapture capture1 = new VideoCapture(0); //create a camera capture
+            Console.WriteLine("Child thread 1 created camera capture");
+            Bitmap image1 = capture1.QueryFrame().ToBitmap(); //take a picture
+            Console.WriteLine("Child thread 1 took a photo");
+            //Saving photos into folder
+            string FileName = System.IO.Path.Combine(@"C:\Users\Admin\Downloads\JC", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            image1.Save(FileName + "1" + ".jpg");
+            Console.WriteLine("Child thread 1 saved an image in the folder");
+        }
+        public static void ChildThread2()
+        {
+            Console.WriteLine("Child 2 thread starts");
+            VideoCapture capture2 = new VideoCapture(1); //create a camera capture
+            Console.WriteLine("Child thread 2 created camera capture");
+            Bitmap image2 = capture2.QueryFrame().ToBitmap(); //take a picture
+            Console.WriteLine("Child thread 2 took a photo");
 
+            string FileName = System.IO.Path.Combine(@"C:\Users\Admin\Downloads\JC", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            image2.Save(FileName + "2" + ".jpg");
+            Console.WriteLine("Child thread 2 saved an image in the folder");
+        }
+
+        public static void ChildThread3()
+        {
+            Console.WriteLine("Child 3 thread starts");
+            VideoCapture capture3 = new VideoCapture(2); //create a camera capture
+            Console.WriteLine("Child thread 3 created camera capture");
+            Bitmap image3 = capture3.QueryFrame().ToBitmap(); //take a picture
+            Console.WriteLine("Child thread 3 took a photo");
+
+            string FileName = System.IO.Path.Combine(@"C:\Users\Admin\Downloads\JC", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            image3.Save(FileName + "3" + ".jpg");
+            Console.WriteLine("Child thread 3 saved an image in the folder");
+        }
+
+        public static void ChildThread4()
+        {
+            Console.WriteLine("Child 4 thread starts");
+            VideoCapture capture4 = new VideoCapture(3); //create a camera capture
+            Console.WriteLine("Child thread 4 created camera capture");
+            Bitmap image4 = capture4.QueryFrame().ToBitmap(); //take a picture
+            Console.WriteLine("Child thread 4 took a photo");
+
+            string FileName = System.IO.Path.Combine(@"C:\Users\Admin\Downloads\JC", DateTime.Now.ToString("yyy-MM-dd-hh-mm-ss"));
+            image4.Save(FileName + "4" + ".jpg");
+            Console.WriteLine("Child thread 4 saved an image in the folder");
+        }
+
+        public static void TakePhotoThreading()
+        {
+            int start = 1;
+            while (start == 1)
+            {
+                System.Threading.ThreadStart childref1 = new ThreadStart(ChildThread1);
+                System.Threading.ThreadStart childref2 = new ThreadStart(ChildThread2);
+                System.Threading.ThreadStart childref3 = new ThreadStart(ChildThread3);
+                System.Threading.ThreadStart childref4 = new ThreadStart(ChildThread4);
+
+                //Console.WriteLine("In Main: Creating the Child threads");
+                Thread childThread1 = new Thread(childref1);
+                childThread1.Start();
+
+                Thread childThread2 = new Thread(childref2);
+                childThread2.Start();
+
+                Thread childThread3 = new Thread(childref3);
+                childThread3.Start();
+
+                Thread childThread4 = new Thread(childref4);
+                childThread4.Start();
+
+                Console.Write("Another image? (1 or 0): ");
+                start = Convert.ToInt32(Console.ReadLine());
             }
         }
 
 
 
-
-    }
+        }
 }
